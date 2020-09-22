322e24ccc3e68845c2bb9782e4a3fb7b062518f0
LautyT02 <LautyT02>
2020-09-22 19:51:07 -0300

    Prueba cambio triviño

--- lib/main.dart
+++ lib/main.dart
@@ -119,5 +119,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-}
-//comentario de prueba. Modificar y subir cambios
+}// Hola. Prueba cambio triviño
