d85167c03123c9164d0bed4249d293f4d5abdf41
franC <Cfrancisco23273@gmail.com>
2020-09-23 21:21:36 -0300

    si explota explota

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_app/screens/autenticacion/auth.dart';
+import 'package:flutter_app/screens/services/auth.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_app/screens/wrapper.dart';
-
+import 'package:flutter_app/models/UserR.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return StreamProvider<UserR>.value(
-      value: AuthService().user,
+      value: AuthService().user, //<= Porque me haces esto AuthService
       child: MaterialApp(
         home: Wrapper(),
       ),
