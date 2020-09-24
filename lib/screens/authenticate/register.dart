a37d64cd52d54ca19ebac12a960592ddbc62cf5f
franC <Cfrancisco23273@gmail.com>
2020-09-23 23:54:09 -0300

    correccion aspectos visuales

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/screens/services/auth.dart';
 import 'package:flutter_app/shared/constants.dart';
 
+//bugs
+//23/09 + cuando se ingresa un correo registrado previamente se lanza el error 
+
 class Register extends StatefulWidget {
 
   final Function toggleView;
@@ -24,77 +27,96 @@ class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.brown[100],
       appBar: AppBar(
-        backgroundColor: Colors.brown[400],
+        backgroundColor: Color.fromARGB(100, 0, 145, 245),
         elevation: 0.0,
-        title: Text('Sign up to Application'),
+        title: Text('COI - ITC'),
         actions: <Widget>[
           FlatButton.icon(
             icon: Icon(Icons.person), 
-            label: Text('Sign In'),
+            label: Text('ingresar'),
             onPressed: (){
               widget.toggleView();
             }
           )
         ],
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                }
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              //color: Colors.grey,
+              alignment: Alignment.center,
+              padding: EdgeInsets.fromLTRB(10, 25, 10, 10),
+              child: Text(
+                'Registro en la aplicacion',
+                style: TextStyle(
+                  color: Color.fromARGB(100, 0, 145, 245),
+                  fontWeight: FontWeight.w500,
+                  fontSize: 28
+                ), 
               ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: 'password'),
-                obscureText: true,
-                validator: (val) => val.length < 6 ? 'Enter a password 6+ chars longs' : null,
-                onChanged: (val) {
-                  setState(() => password = val);
-                }
-              ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                onPressed: () async {
-                  // check the form for empty spots and validity
-                  bool emailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(email);
-                  if (emailValid) {
-                    if (_formKey.currentState.validate()) {
-                      dynamic result = await _auth.registerWhitEmailAndPassword(email, password);
-                      print(result);
-                      if (result == null) {
-                        setState(() => error = 'Write a valid email');
-                      }
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 50.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    decoration: textInputDecoration.copyWith(labelText: 'Mail',hintText: 'ejemplo@mail.com'),
+                    validator: (val) => val.length < 6 ? 'ingrese un mail' : null,
+                    onChanged: (val) {
+                      setState(() => email = val);
                     }
-                  } else {
-                    setState(
-                        () => error = 'Write a valid email you moron!');
-                  }
-                },
-                color: Colors.pink[300],
-                child: Text(
-                  'Register',
-                  style: TextStyle(color: Colors.white),
-                )
+                  ),
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    decoration: textInputDecoration.copyWith(labelText: 'Contraseña',hintText: '******'),
+                    obscureText: true,
+                    validator: (val) => val.length < 6 ? 'ingrese una clave de mas de 6 digitos' : null,
+                    onChanged: (val) {
+                      setState(() => password = val);
+                    }
+                  ),
+                  SizedBox(height: 20.0),
+                  RaisedButton(
+                    onPressed: () async {
+                      // check the form for empty spots and validity
+                      bool emailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(email);
+                      if (emailValid) {
+                        if (_formKey.currentState.validate()) {
+                          dynamic result = await _auth.registerWhitEmailAndPassword(email, password);
+                          print(result);
+                          if (result == null) {
+                            setState(() => error = 'Write a valid email');
+                          }
+                        }
+                      } 
+                      /*else {
+                        setState(() => error = 'ingresar un mail valido');
+                      }*/
+                    },
+                    color: Colors.pink[300],
+                    child: Text(
+                      'Register',
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ),
+                  SizedBox(height: 12.0),
+                  Text(
+                    error,
+                    style: TextStyle(color: Colors.red, fontSize: 14.0),
+                  ),
+                  ],
+                ),
               ),
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
-              ),
-            ],
-          ),
+            ),
+          ]
         ),
-      ),
+      )
     );
   }
 }
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_app/screens/services/auth.dart';
 import 'package:flutter_app/shared/constants.dart';
@@ -25,101 +27,104 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(    //loading ? Loading() : 
-      backgroundColor: Colors.brown[100],
       appBar: AppBar(
-        backgroundColor: Colors.brown[400],
+        backgroundColor: Color.fromARGB(100, 0, 145, 245),
         elevation: 0.0,
-        title: Text('Sign in to Application'),
+        title: Text('Inicio de Sesion'),
         actions: <Widget>[
           FlatButton.icon(
             icon: Icon(Icons.person), 
-            label: Text('Register'),
+            label: Text('Registrarse'),
             onPressed: (){
               widget.toggleView();
             }
           )
         ],
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                }
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: 'Password'),
-                obscureText: true,
-                validator: (val) => val.length < 6 ? 'Enter a password 6+ chars longs' : null,
-                onChanged: (val) {
-                  setState(() => password = val);
-                }
-              ),
-              SizedBox(height: 20.0),
-              /*
-              RaisedButton(
-                onPressed: () async {
-                  // check the form for empty spots and validity
-                  bool emailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(email);
-                  if (emailValid) {
-                    if (_formKey.currentState.validate()) {
-                      setState(() => loading = false);
-                      dynamic result = await _auth.SignInWhitEmailAndPassword(email, password);
-                      print(result);
-                      if (result == null) {
-                      setState(() => error = 'Write a valid email');
-                      loading = false;
+      body: ListView(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 50.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    decoration: textInputDecoration.copyWith(hintText: 'Email'),
+                    validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                    onChanged: (val) {
+                      setState(() => email = val);
                     }
-                  } else {
-                    setState(
-                        () => error = 'Write a valid email you moron!');
-                        loading = false;
-                  }
-                }},
-                color: Colors.pink[300],
-                child: Text(
-                  'Sign in ',
-                  style: TextStyle(color: Colors.white),
-                )
-              ),*/
-              ///*
-              RaisedButton(
-                color: Colors.pink[400],
-                child: Text(
-                  'sign in',
-                  style: TextStyle(color: Colors.white),
-                ),
-                
-                onPressed: () async {
-                  if (_formKey.currentState.validate()){
-                    setState(() => loading = true);
-                    dynamic result = await _auth.SignInWhitEmailAndPassword(email, password);
-                    if(result == null){ //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++EVALUAR PORQUE NO CAPTURA EL ERROR
-                      setState(() {
-                        error = 'could not sign in with those credentials';
-                        loading = false;
-                      });
+                  ),
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    decoration: textInputDecoration.copyWith(hintText: 'Password'),
+                    obscureText: true,
+                    validator: (val) => val.length < 6 ? 'Enter a password 6+ chars longs' : null,
+                    onChanged: (val) {
+                      setState(() => password = val);
                     }
-                  }
-                }
-                
-              ),//*/
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
+                  ),
+                  SizedBox(height: 20.0),
+                  /*
+                  RaisedButton(
+                    onPressed: () async {
+                      // check the form for empty spots and validity
+                      bool emailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(email);
+                      if (emailValid) {
+                        if (_formKey.currentState.validate()) {
+                          setState(() => loading = false);
+                          dynamic result = await _auth.SignInWhitEmailAndPassword(email, password);
+                          print(result);
+                          if (result == null) {
+                          setState(() => error = 'Write a valid email');
+                          loading = false;
+                        }
+                      } else {
+                        setState(
+                            () => error = 'Write a valid email you moron!');
+                            loading = false;
+                      }
+                    }},
+                    color: Colors.pink[300],
+                    child: Text(
+                      'Sign in ',
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ),*/
+                  ///*
+                  RaisedButton(
+                    color: Colors.pink[400],
+                    child: Text(
+                      'sign in',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    
+                    onPressed: () async {
+                      if (_formKey.currentState.validate()){
+                        setState(() => loading = true);
+                        dynamic result = await _auth.SignInWhitEmailAndPassword(email, password);
+                        if(result == null){ //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++EVALUAR PORQUE NO CAPTURA EL ERROR
+                          setState(() {
+                            error = 'could not sign in with those credentials';
+                            loading = false;
+                          });
+                        }
+                      }
+                    }
+                    
+                  ),//*/
+                  SizedBox(height: 12.0),
+                  Text(
+                    error,
+                    style: TextStyle(color: Colors.red, fontSize: 14.0),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
